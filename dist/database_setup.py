9a38b806cf783fa796b341a2ffca0692d47ca022
Tiffany Han <hantiffany@gmail.com>
2016-08-30 10:51:43 -0700

    change directory file structure to include separate src and dist folders

---
+++ dist/database_setup.py
@@ -0,0 +1,69 @@
+# starting configuration files for sqlalchemy
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+# define classes here
+class User(Base):
+	__tablename__ = 'user'
+
+	user_id = Column(Integer, primary_key = True)
+	name = Column(String(80), nullable = False)
+	email = Column(String(80), nullable = False)
+	picture = Column(String(250))
+	restaurant = relationship('Restaurant', cascade='all, delete-orphan')
+	menu_item = relationship('MenuItem', cascade='all, delete-orphan')
+
+
+class Restaurant(Base):
+	__tablename__ = 'restaurant'
+
+	name = Column(String(80),
+				  nullable = False)
+	restaurant_id = Column(Integer,
+						   primary_key = True)
+	user_id = Column(Integer, ForeignKey('user.user_id'))
+	menu_item = relationship('MenuItem', cascade='all, delete-orphan')
+	# add a method to help return a JSON endpoint
+	# of the restaurant object's properties
+	@property
+	def serialize(self):
+		return {
+			'name': self.name,
+			'restaurant_id': self.restaurant_id
+		}
+
+
+class MenuItem(Base):
+	__tablename__ = 'menu_item'
+
+	name = Column(String(80),
+				  nullable = False)
+	menu_id = Column(Integer,
+					 primary_key = True)
+	course = Column(String(250))
+	description = Column(String(250))
+	price = Column(String(8))
+	restaurant_id = Column(Integer,
+						   ForeignKey('restaurant.restaurant_id'))
+	user_id = Column(Integer, ForeignKey('user.user_id'))
+	# add a method to help return a JSON endpoint
+	# of the menu item object's properties
+	@property
+	def serialize(self):
+		return {
+			'name': self.name,
+			'description': self.description,
+			'id': self.menu_id,
+			'price': self.price,
+			'course': self.course
+		}
+
+
+# Ending configuration files
+engine = create_engine('postgresql://catalog:catalog@localhost/catalog')
+Base.metadata.create_all(engine)
